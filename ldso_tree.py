478ee23b702e6b71024ac42a0448b54fa168453e
Artemish <mitch.cro@gmail.com>
2018-06-27 00:01:08 -0500

    Made ldso_tree.py function as a script

--- ldso_tree.py
+++ ldso_tree.py
@@ -1,5 +1,7 @@
 import sh
 import collections
+import sys
+import pprint
 
 def process(lib):
   output = sh.ldd(lib)
@@ -25,7 +27,7 @@ def walk(start):
     while len(exploration_queue) > 0:
         parent_lib, parent_path = exploration_queue.pop()
         seen.add(parent_lib)
-        print("Exploring " + parent_lib)
+        # print("Exploring " + parent_lib)
         for child_lib, child_path in process(parent_path):
             graph[child_lib]['parent'] = parent_lib
 
@@ -33,3 +35,21 @@ def walk(start):
                 exploration_queue.append((child_lib, child_path))
 
     return graph
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ldso_walker.py <executable>")
+        print("")
+        print("       Uses ldd to gather the runtime")
+        print("       dependencies for the given executable")
+        print("       and print out the tree. This is useful")
+        print("       for determining the 'core' requirements")
+        print("       for statically compiling the binary")
+        sys.exit(1)
+
+    graph = walk(sys.argv[1])
+
+    base_deps = [lib for lib in graph if graph[lib]['parent'] == 'Base']
+    print("Base dependencies:")
+    for dep in base_deps:
+        print(dep)
