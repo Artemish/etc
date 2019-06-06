c40f67d7ecec035066e1070d379c537cdc34cdc6
Artemish <mitch.cro@gmail.com>
2019-06-06 11:28:24 -0500

    Update bash aliases, completion for git aliases

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -6,5 +6,19 @@ alias ic='ping google.com'
 alias lsd='ls -d */'
 alias svnlog='svn log -v --limit 6 | less'
 alias fnd='find ./ -name '
-alias notes='vim ~/notes.txt'
-alias reload='source ~/.bashrc'
+
+alias vi=nvim
+alias vim=nvim
+
+alias gst='git status'
+alias gc='git commit'
+alias gco='git checkout'
+
+if [ -f /usr/share/bash-completion/completions/git ]; then
+  . /usr/share/bash-completion/completions/git
+  __git_complete gst _git_status
+  __git_complete gc  _git_commit
+  __git_complete gco _git_checkout
+fi
+
+alias short='grep -Ev ".{120}"'
